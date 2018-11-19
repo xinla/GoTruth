f57a4ccc319b018ae81c222dfa4580780c790cb3
xinla <1570728529@qq.com>
2018-11-19 22:46:35 +0800

    腾讯地图相关组件

---
+++ App/src/service/util/mapUtilTx.js
@@ -0,0 +1,40 @@
+var mapUtil = {
+
+};
+
+/**
+ * 加载地图
+ */
+mapUtil.load = function(call) {
+
+	adaptHeight(); //动态适配高度
+
+	window.onresize = function() { //横屏、QQ浏览器变全屏模式下的时候，需要重新计算高度              
+		adaptHeight();
+	}
+
+	window.addEventListener('message', function(event) {
+		var data = event.data;
+	//	alert('你使用的组件是' + loc.module + ',刚选择了' + loc.poiname + ',它位于' + loc.poiaddress + ',它的经纬度是：' + loc.latlng.lat + ',' + loc.latlng.lng + ',所属城市为:' + loc.cityname)
+		
+		var resMap = {};
+		resMap.publishaddresses = data.poiname
+		resMap.latitude = data.latlng.lat;
+		resMap.longitude = data.latlng.lng;
+		resMap.publishstreet = data.poiaddress
+		// console.log(data);
+		call(resMap);
+		
+	}, false);
+
+	function adaptHeight() {
+		var winH = $(window).height();
+		var bodyH = document.documentElement.clientHeight;
+		if(winH > bodyH) {
+			window.parent.document.getElementById("iframe").height = winH;
+		} else {
+			window.parent.document.getElementById("iframe").height = bodyH;
+		}
+	}
+}
+export default mapUtil
---
+++ App/src/view/member/positionTx.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<iframe id="iframe" width="100%" frameborder=0 scrolling="no"
+        src="//apis.map.qq.com/tools/locpicker?search=1&type=1&key=OB4BZ-D4W3U-B7VVO-4PJWW-6TKDJ-WPB77&referer=myapp">
+	    </iframe>
+	</div>
+</template>
+
+<script>
+import '@/service/util/txapi'
+import mapUtilTx from '@/service/util/mapUtilTx'
+export default {
+	mounted(){
+		let _this = this;
+		mapUtilTx.load(function(data){
+			_this.$store.commit('setSelectedPublishName',data.publishaddresses);
+			_this.$store.commit('setSelectedPublishAddress',data.publishstreet)
+			_this.$Tool.goBack();
+			//{publishaddresses: "君悦玺园", latitude: 30.66268, longitude: 117.48011, publishstreet: "安徽省池州市贵池区君悦广场(百牙西路南)"}
+		})
+	}
+}
+</script>
