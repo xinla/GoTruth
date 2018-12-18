852716db80ba4dcfca1a4354856d1d58e5c612b3
Deepbooty <511108087@qq.com>
2018-12-18 17:18:47 +0800

    路径

--- App/src/lib/config/config.js
+++ App/src/lib/config/config.js
@@ -4,7 +4,8 @@ const fileServer = "http://212.64.1.189:9180/zjzx_file"
 const fileRoot = "http://212.64.1.189:9180/fileserver"*/
 
 const domain = "http://192.168.2.103:8080";
-const successServer = "http://47.101.58.43"
+// const successServer = "http://47.101.58.43"
+const successServer = "http://www.kangyoubao.net:8585"
 const fileServer = "http://47.101.58.43:8081"
 const fileRoot = "http://47.101.58.43:8085"
 export default {
