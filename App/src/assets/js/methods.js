39826ec419010c3fea7af5685ed130e3ed69237a
xinla <1570728529@qq.com>
2019-02-20 19:38:31 +0800

    点赞功能修改，注释iconfont Unicode引用等

--- App/src/assets/js/methods.js
+++ App/src/assets/js/methods.js
@@ -307,12 +307,12 @@ const tool = {
      */
     numConvertText(num){
         if (typeof num !== "number") {return;}
-        if (num >= 1000) {
-            if (num >= 10000) {
-                return (num/10000).toFixed(1) + 'w';
-            }
-            return (num/1000).toFixed(1) + 'k';
+        if (num >= 10000) {
+            return (num/10000).toFixed(1) + 'w';
         }
+        /*if (num >= 1000) {
+            return (num/1000).toFixed(1) + 'k';
+        }*/
         return num;
     }
 }
